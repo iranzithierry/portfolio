297eebcc65d3f0310e0e6500e526895d5f280ea1
Iranzi Dev <iranzithierry12@gmail.com>
2024-01-21 17:30:01 +0200

    sticky coe

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,17 +32,24 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={cn('font-sans antialiased', GeistSans.variable, GeistMono.variable)}>
-        <div className='fixed h-screen w-full z-10 bg-gradient-to-br from-indigo-50  via-white  to-cyan-100' />
+        <div className='fixed min-h-[110vh] w-full z-10 bg-gradient-to-br from-indigo-50  via-white  to-cyan-100' />
         <Toaster />
         <Providers attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           {/* @ts-ignore */}
           <div className='relative z-40'>
-          <Suspense fallback="...">
-            <Header />
-          </Suspense>
+            <Suspense fallback="...">
+              <Header />
+            </Suspense>
             <main className="flex flex-col flex-1">
               {children}
               <Analytics />
+              <div className=" sticky bottom-0 left-0 right-0 z-50 flex h-12 w-full flex-wrap justify-center 2xl:h-14 active animation">
+                <div className="relative mx-auto flex h-10 shrink flex-wrap items-center justify-center rounded-full border-1/2 border-slate-200 bg-primary px-5 py-1 text-sm  text-white shadow-xl 2xl:h-14">
+                  <a href="https://www.buymeacoffee.com/iranzithierry" target="_blank" rel="noopener noreferrer" className="mx-auto flex max-w-fit items-center justify-center rounded-lg transition-all duration-75 hover:scale-105">
+                    <img className='h-8' src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=iranzithierry&button_colour=0069ff&font_colour=ffffff&font_family=Inter&outline_colour=ffffff&coffee_colour=FFDD00" />
+                  </a>
+                </div>
+              </div>
             </main>
             <Footer />
           </div>
--- components/footer.tsx
+++ components/footer.tsx
@@ -2,14 +2,11 @@ import { ExternalLink } from '@/components/ui/external-link'
 
 export default function Footer() {
   return (
-    <div className="w-full py-4 text-center">
+    <div className="w-full py-4 text-center pb-16">
       <p className="text-gray-500 text-xs">
-      All Rights Reserved. Developed by{" "}
+        All Rights Reserved. Developed by{" "}
         <span className='font-semibold'>Iranzi Dev</span>
       </p>
-      <a href="https://www.buymeacoffee.com/iranzithierry" target="_blank" rel="noopener noreferrer" className="mx-auto mt-2 flex max-w-fit items-center justify-center rounded-lgtransition-all duration-75 hover:scale-105">
-        <img className='h-8' src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=iranzithierry&button_colour=0069ff&font_colour=ffffff&font_family=Inter&outline_colour=ffffff&coffee_colour=FFDD00" />
-      </a>
     </div>
   );
 }
