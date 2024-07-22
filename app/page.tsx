5c556f221bc886eadaf7c87a9bb1dbb9410bf37b
Iranzi Thierry <115815954+iranzithierry@users.noreply.github.com>
2024-07-22 20:08:51 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import Container from '@/components/container'
 import Profile from '@/components/profile'
-import { redirect } from 'next/navigation'
 import React from 'react'
 
 export default function Page() {
