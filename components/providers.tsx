8f9fec15d32ee7c0923ad0b84278aeada10ec71f
Troy <iranzithierry12@gmail.com>
2024-12-26 20:19:58 +0200

    add posthog

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import * as React from "react"
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
 import { type ThemeProviderProps } from "next-themes/dist/types"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: "always"
+  })
+}
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider {...props}>
+      <PostHogProvider client={posthog}>
         {children}
+      </PostHogProvider>
     </NextThemesProvider>
   )
 }
