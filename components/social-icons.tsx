41e55e486321d706d502c64529740b01ea4a1e23
Troy <iranzithierry12@gmail.com>
2025-01-09 13:47:11 +0200

    Update social-icons.tsx

--- components/social-icons.tsx
+++ components/social-icons.tsx
@@ -21,7 +21,7 @@ const socials = [
     },
     {
         name: "X",
-        href: "https://x.com/iranzithierry12",
+        href: "https://x.com/intent/follow?original_referer=https://publish.x.com/&screen_name=iranzithierry12",
         icon: (props: any) => (
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 25" data-slot="icon" aria-hidden="true" {...props}>
                 <path fill="currentColor" d="M17.403 4.25h2.882l-6.296 7.201 7.407 9.799h-5.8l-4.542-5.943-5.198 5.943H2.973l6.734-7.702L2.602 4.25h5.946l4.106 5.432 4.75-5.432Zm-1.011 15.274h1.597L7.68 5.885H5.967z"></path>
