ba1b05227c682f3cc1163aa70a8544f87261020b
Troy <iranzithierry12@gmail.com>
2025-06-23 13:13:37 +0200

    theme transition

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,25 +1,42 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from "next-themes"
+import * as React from "react";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export function ThemeToggle() {
-    const { setTheme, theme } = useTheme()
+  const { setTheme, theme } = useTheme();
+  const switchTheme: () => void = () => {
+    switch (theme) {
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("light");
+        break;
+      default:
+        break;
+    }
+  };
+  const toggleTheme = () => {
+    //@ts-ignore
+    if (!document.startViewTransition) switchTheme();
 
-    return (
-        <Button
-            appearance="plain"
-            shape="circle"
-            intent="secondary"
-            size="square-petite"
-            onPress={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-            <Sun className="text-gray-500 dark:text-gray-300 h-4 w-4 dark:hidden" />
-            <Moon className="text-gray-500 dark:text-gray-300 hidden h-4 w-4 dark:block" />
-            <span className="sr-only">Toggle theme</span>
-        </Button>
-    )
+    //@ts-ignore
+    document.startViewTransition(switchTheme);
+  };
+  return (
+    <Button
+      appearance="plain"
+      shape="circle"
+      intent="secondary"
+      size="square-petite"
+      onPress={toggleTheme}>
+      <SunIcon className="size-4 text-gray-500 dark:text-gray-300 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute size-4 text-gray-500 dark:text-gray-300 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
 }
